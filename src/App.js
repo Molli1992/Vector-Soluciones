0b7927f0b9af5eda50a9d4ca623e7b251d7e18fc
molli1992 <fblaksley@rukasvancor.com>
2023-09-21 17:09:08 -0300

    componente inicio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./app.css"
+import "@fontsource/montserrat"; // Defaults to weight 400
+import "@fontsource/montserrat/400.css"; // Specify weight
+import "@fontsource/montserrat/400-italic.css"; // Specify weight and style
 import Header from "./componentes/header/header";
 import Inicio from "./componentes/inicio/inicio";
 
--- src/componentes/inicio/inicio.jsx
+++ src/componentes/inicio/inicio.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./inicio.css";
 
 function Inicio() {
-  return <div>Inicio</div>;
+  return (
+    <div className="body-inicio">
+      <div className="container-inicio-1">
+        <div className="container-inicio-1-container">
+          <div className="container-inicio-1-container-text">
+            <h1>Unión Ads Technology</h1>
+            <h3>Solución en cobranza</h3>
+            <div>
+              <text>
+                Simplifica y optimiza tus procesos financieros con nuestra
+                solución de gestión de cobros y pagos. Ahorra tiempo y reduce el
+                estrés de la gestión financiera
+              </text>
+            </div>
+            <button>Quienes Somos</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Inicio;
