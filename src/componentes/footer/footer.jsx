6811f153e7e4cfa2c9837b25731d4966fabbcc04
molli1992 <fblaksley@rukasvancor.com>
2023-10-17 18:05:15 -0300

    location

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./footer.css";
 
 function Footer() {
+  const location = useLocation();
   const onClick = () => {
     window.scrollTo({
       top: 0,
@@ -58,53 +59,57 @@ function Footer() {
     });
   };
 
-  return (
-    <div className="body-footer">
-      <div className="container-1-footer">
-        <Link
-          to={"/quienes-somos"}
-          className="link-footer"
-          onClick={onClickNosotros}
-        >
-          Quienes Somos
-        </Link>
-        <div className="line-footer"></div>
-        <Link
-          to={"/pago-proveedores"}
-          className="link-footer"
-          onClick={onClickProveedores}
-        >
-          Pago Proveedores
-        </Link>
-        <div className="line-footer"></div>
-        <Link
-          to={"/cobranzas-regulares"}
-          className="link-footer"
-          onClick={onClickCobranzas}
-        >
-          Cobranzas Regulares
-        </Link>
+  if (location.pathname === "/link") {
+    return null;
+  } else {
+    return (
+      <div className="body-footer">
+        <div className="container-1-footer">
+          <Link
+            to={"/quienes-somos"}
+            className="link-footer"
+            onClick={onClickNosotros}
+          >
+            Quienes Somos
+          </Link>
+          <div className="line-footer"></div>
+          <Link
+            to={"/pago-proveedores"}
+            className="link-footer"
+            onClick={onClickProveedores}
+          >
+            Pago Proveedores
+          </Link>
+          <div className="line-footer"></div>
+          <Link
+            to={"/cobranzas-regulares"}
+            className="link-footer"
+            onClick={onClickCobranzas}
+          >
+            Cobranzas Regulares
+          </Link>
+        </div>
+        <div className="container-2-footer">
+          <text>© 2023 Vector Soluciones Todos los derechos reservados.</text>
+        </div>
+        <div className="arrow-container-footer" onClick={onClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            fill="white"
+            class="bi bi-arrow-up"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
+            />
+          </svg>
+        </div>
       </div>
-      <div className="container-2-footer">
-        <text>© 2023 Vector Soluciones Todos los derechos reservados.</text>
-      </div>
-      <div className="arrow-container-footer" onClick={onClick}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="22"
-          height="22"
-          fill="white"
-          class="bi bi-arrow-up"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
-          />
-        </svg>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Footer;
--- src/componentes/header/header.jsx
+++ src/componentes/header/header.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "../../logo/logo-vector-3-removebg-preview.png";
 import { useNavigate } from "react-router-dom";
 import "./header.css";
 
 function Header() {
+  const location = useLocation();
   const [state, setState] = useState(false);
   const navigate = useNavigate();
 
@@ -78,133 +79,137 @@ function Header() {
     cobranzas.classList.add("active");
   };
 
-  return (
-    <div className="body-header">
-      <div className="header-container-left">
-        <img src={Logo} alt="Logo" />
-      </div>
-
-      <div className="header-container-rigth">
-        <Link
-          id="inicio"
-          to={"/"}
-          className="link-header active"
-          onClick={activeInicio}
-        >
-          Inicio
-        </Link>
-
-        <Link
-          id="nosotros"
-          to={"quienes-somos"}
-          className="link-header"
-          onClick={activeNosotros}
-        >
-          Quienes Somos
-        </Link>
-
-        <Link
-          id="proveedores"
-          to={"pago-proveedores"}
-          className="link-header"
-          onClick={activeProveedores}
-        >
-          Pago Proveedores
-        </Link>
-
-        <Link
-          id="cobranzas"
-          to={"cobranzas-regulares"}
-          className="link-header"
-          onClick={activeCobranzas}
-        >
-          Cobranzas Regulares
-        </Link>
-
-        <button onClick={onClickRoute}>Contacto</button>
-      </div>
-
-      <div className="svg-container" onClick={onClick}>
-        {state === false ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            fill="currentColor"
-            class="bi bi-list"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-x-lg"
-            viewBox="0 0 16 16"
-          >
-            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-          </svg>
-        )}
-      </div>
-
-      {state === true ? (
-        <div className="header-container-responsive">
-          <div className="container-link-responsive">
-            <Link
-              id="inicio-2"
-              to={"/"}
-              className="link-header-responsive"
-              onClick={() => {
-                onClick();
-              }}
-            >
-              Inicio
-            </Link>
-
-            <Link
-              id="nosotros-2"
-              to={"quienes-somos"}
-              className="link-header-responsive"
-              onClick={() => {
-                onClick();
-              }}
-            >
-              Quienes Somos
-            </Link>
-
-            <Link
-              id="proveedores-2"
-              to={"pago-proveedores"}
-              className="link-header-responsive"
-              onClick={() => {
-                onClick();
-              }}
-            >
-              Pago Proveedores
-            </Link>
-
-            <Link
-              id="cobranzas-2"
-              to={"cobranzas-regulares"}
-              className="link-header-responsive"
-              onClick={() => {
-                onClick();
-              }}
-            >
-              Cobranzas Regulares
-            </Link>
-            <button onClick={onClickRoute}>Contacto</button>
-          </div>
+  if (location.pathname === "/link") {
+    return null;
+  } else {
+    return (
+      <div className="body-header">
+        <div className="header-container-left">
+          <img src={Logo} alt="Logo" />
         </div>
-      ) : null}
-    </div>
-  );
+
+        <div className="header-container-rigth">
+          <Link
+            id="inicio"
+            to={"/"}
+            className="link-header active"
+            onClick={activeInicio}
+          >
+            Inicio
+          </Link>
+
+          <Link
+            id="nosotros"
+            to={"quienes-somos"}
+            className="link-header"
+            onClick={activeNosotros}
+          >
+            Quienes Somos
+          </Link>
+
+          <Link
+            id="proveedores"
+            to={"pago-proveedores"}
+            className="link-header"
+            onClick={activeProveedores}
+          >
+            Pago Proveedores
+          </Link>
+
+          <Link
+            id="cobranzas"
+            to={"cobranzas-regulares"}
+            className="link-header"
+            onClick={activeCobranzas}
+          >
+            Cobranzas Regulares
+          </Link>
+
+          <button onClick={onClickRoute}>Contacto</button>
+        </div>
+
+        <div className="svg-container" onClick={onClick}>
+          {state === false ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              class="bi bi-list"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-x-lg"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+            </svg>
+          )}
+        </div>
+
+        {state === true ? (
+          <div className="header-container-responsive">
+            <div className="container-link-responsive">
+              <Link
+                id="inicio-2"
+                to={"/"}
+                className="link-header-responsive"
+                onClick={() => {
+                  onClick();
+                }}
+              >
+                Inicio
+              </Link>
+
+              <Link
+                id="nosotros-2"
+                to={"quienes-somos"}
+                className="link-header-responsive"
+                onClick={() => {
+                  onClick();
+                }}
+              >
+                Quienes Somos
+              </Link>
+
+              <Link
+                id="proveedores-2"
+                to={"pago-proveedores"}
+                className="link-header-responsive"
+                onClick={() => {
+                  onClick();
+                }}
+              >
+                Pago Proveedores
+              </Link>
+
+              <Link
+                id="cobranzas-2"
+                to={"cobranzas-regulares"}
+                className="link-header-responsive"
+                onClick={() => {
+                  onClick();
+                }}
+              >
+                Cobranzas Regulares
+              </Link>
+              <button onClick={onClickRoute}>Contacto</button>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default Header;
